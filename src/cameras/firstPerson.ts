7a2a55038f89f776e5c45d12b8b6b384f8d58bfb
amphineko <sukareki@gmail.com>
2022-02-05 03:04:51 +0000

    fix(fpcamera): translate should respect current rotation

--- src/cameras/firstPerson.ts
+++ src/cameras/firstPerson.ts
@@ -19,10 +19,10 @@ export class FirstPersonCamera {
 
     #view: mat4 = mat4.create()
 
-    keyboardInterval = 0
-    keyboardLastTime = 0
-    keyboardNegative = vec3.fromValues(0, 0, 0)
-    keyboardPositive = vec3.fromValues(0, 0, 0)
+    #keyboardInterval = 0
+    #keyboardLastTime = 0
+    #keyboardNegative = vec3.fromValues(0, 0, 0)
+    #keyboardPositive = vec3.fromValues(0, 0, 0)
 
     #keydownHandler: (e: KeyboardEvent) => void
     #keyupHandler: (e: KeyboardEvent) => void
@@ -88,68 +88,73 @@ export class FirstPersonCamera {
         this.#updateView()
     }
 
-    translate(offset: vec3) {
-        vec3.add(this.#position, this.#position, offset)
+    /**
+     * Translate the camera relatively to the current rotation
+     */
+    translateRelative(offset: vec3) {
+        vec3.scaleAndAdd(this.#position, this.#position, this.#front, -offset[2])
+        vec3.scaleAndAdd(this.#position, this.#position, this.#right, -offset[0])
+        vec3.scaleAndAdd(this.#position, this.#position, this.#up, offset[1])
         this.#updateView()
     }
 
     #handleKeydown(e: KeyboardEvent) {
         if (e.key === 'w' || e.key === 'W') {
-            this.keyboardNegative[2] = keyboardTranslateRate
+            this.#keyboardNegative[2] = keyboardTranslateRate
         }
         if (e.key === 'a' || e.key === 'A') {
-            this.keyboardNegative[0] = keyboardTranslateRate
+            this.#keyboardNegative[0] = keyboardTranslateRate
         }
         if (e.key === 'f' || e.key === 'F') {
-            this.keyboardNegative[1] = keyboardTranslateRate
+            this.#keyboardNegative[1] = keyboardTranslateRate
         }
         if (e.key === 's' || e.key === 'S') {
-            this.keyboardPositive[2] = keyboardTranslateRate
+            this.#keyboardPositive[2] = keyboardTranslateRate
         }
         if (e.key === 'd' || e.key === 'D') {
-            this.keyboardPositive[0] = keyboardTranslateRate
+            this.#keyboardPositive[0] = keyboardTranslateRate
         }
         if (e.key === 'r' || e.key === 'R') {
-            this.keyboardPositive[1] = keyboardTranslateRate
+            this.#keyboardPositive[1] = keyboardTranslateRate
         }
 
-        if (vec3.distance(this.keyboardNegative, zero) > 0 || vec3.distance(this.keyboardPositive, zero) > 0) {
-            this.keyboardLastTime = performance.now()
-            this.keyboardInterval = setInterval(() => {
-                const deltaTime = (performance.now() - this.keyboardLastTime) / 1000
+        if (vec3.distance(this.#keyboardNegative, zero) > 0 || vec3.distance(this.#keyboardPositive, zero) > 0) {
+            this.#keyboardLastTime = performance.now()
+            this.#keyboardInterval = setInterval(() => {
+                const deltaTime = (performance.now() - this.#keyboardLastTime) / 1000
                 const offset = vec3.fromValues(
-                    (-this.keyboardNegative[0] + this.keyboardPositive[0]) * deltaTime,
-                    (-this.keyboardNegative[1] + this.keyboardPositive[1]) * deltaTime,
-                    (-this.keyboardNegative[2] + this.keyboardPositive[2]) * deltaTime
+                    (-this.#keyboardNegative[0] + this.#keyboardPositive[0]) * deltaTime,
+                    (-this.#keyboardNegative[1] + this.#keyboardPositive[1]) * deltaTime,
+                    (-this.#keyboardNegative[2] + this.#keyboardPositive[2]) * deltaTime
                 )
-                this.translate(offset)
-                this.keyboardLastTime = performance.now()
+                this.translateRelative(offset)
+                this.#keyboardLastTime = performance.now()
             }) as unknown as number
         }
     }
 
     #handleKeyup(e: KeyboardEvent) {
         if (e.key === 'w' || e.key === 'W') {
-            this.keyboardNegative[2] = 0
+            this.#keyboardNegative[2] = 0
         }
         if (e.key === 'a' || e.key === 'A') {
-            this.keyboardNegative[0] = 0
+            this.#keyboardNegative[0] = 0
         }
         if (e.key === 'f' || e.key === 'F') {
-            this.keyboardNegative[1] = 0
+            this.#keyboardNegative[1] = 0
         }
         if (e.key === 's' || e.key === 'S') {
-            this.keyboardPositive[2] = 0
+            this.#keyboardPositive[2] = 0
         }
         if (e.key === 'd' || e.key === 'D') {
-            this.keyboardPositive[0] = 0
+            this.#keyboardPositive[0] = 0
         }
         if (e.key === 'r' || e.key === 'R') {
-            this.keyboardPositive[1] = 0
+            this.#keyboardPositive[1] = 0
         }
 
-        if (vec3.distance(this.keyboardNegative, zero) === 0 || vec3.distance(this.keyboardPositive, zero) === 0) {
-            clearInterval(this.keyboardInterval)
+        if (vec3.distance(this.#keyboardNegative, zero) === 0 || vec3.distance(this.#keyboardPositive, zero) === 0) {
+            clearInterval(this.#keyboardInterval)
         }
     }
 
