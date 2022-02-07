34c876be424220a13bac409748b10133cc24962e
amphineko <sukareki@gmail.com>
2022-02-07 16:51:08 +0000

    feat(cs7gv5/propeller): plane body rotation

--- pages/cs7gv5/propeller.tsx
+++ pages/cs7gv5/propeller.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useRef } from 'react'
-import { Object3D, PerspectiveCamera, PointLight, Scene, sRGBEncoding, WebGLRenderer } from 'three'
+import { Euler, Object3D, PerspectiveCamera, PointLight, Scene, sRGBEncoding, WebGLRenderer } from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { degToRad } from 'three/src/math/MathUtils'
 
 const gltfLoader = new GLTFLoader()
 const dracoLoader = new DRACOLoader()
@@ -11,6 +12,7 @@ gltfLoader.setDRACOLoader(dracoLoader)
 const PropellerPage = () => {
     const canvas = useRef<HTMLCanvasElement>()
     const container = useRef<HTMLDivElement>()
+    const fpsIndicator = useRef<HTMLSpanElement>()
 
     useEffect(() => {
         if (!canvas.current) {
@@ -39,7 +41,9 @@ const PropellerPage = () => {
         // camera initialization
 
         const camera = new PerspectiveCamera(75, window?.innerWidth / window?.innerHeight, 0.1, 1000)
-        camera.position.z = 5
+        camera.position.x = 2.5
+        camera.position.y = 2.5
+        camera.position.z = -2.5
 
         const light = new PointLight(0xffffff, 1, 100)
         camera.add(light)
@@ -53,42 +57,130 @@ const PropellerPage = () => {
         // scene initialization
 
         const scene = new Scene()
+        let plane: Object3D | null = null
         let propeller: Object3D | null = null
         scene.add(camera)
         gltfLoader.load(
             '/models/propeller/scene.gltf',
             (gltf) => {
                 scene.add(gltf.scene)
+                plane = gltf.scene.getObjectByName('Root')
                 propeller = gltf.scene.getObjectByName('Propeller')
             },
             (xhr) => console.log(`${(xhr.loaded / xhr.total) * 100}% loaded`),
             (error) => console.error(error)
         )
 
+        // keyboard controls
+
+        const keydownTimestamp = { pX: 0, pY: 0, pZ: 0, nX: 0, nY: 0, nZ: 0 }
+        const onKeydown = (event: KeyboardEvent) => {
+            if ((event.key === 'w' || event.key === 'W') && keydownTimestamp.nZ === 0) {
+                keydownTimestamp.pZ = performance.now()
+            }
+            if ((event.key === 's' || event.key === 'S') && keydownTimestamp.pZ === 0) {
+                keydownTimestamp.nZ = performance.now()
+            }
+            if ((event.key === 'a' || event.key === 'A') && keydownTimestamp.nX === 0) {
+                keydownTimestamp.pX = performance.now()
+            }
+            if ((event.key === 'd' || event.key === 'D') && keydownTimestamp.pX === 0) {
+                keydownTimestamp.nX = performance.now()
+            }
+            if ((event.key === 'q' || event.key === 'Q') && keydownTimestamp.nY === 0) {
+                keydownTimestamp.pY = performance.now()
+            }
+            if ((event.key === 'e' || event.key === 'E') && keydownTimestamp.pY === 0) {
+                keydownTimestamp.nY = performance.now()
+            }
+
+            if (event.key === ' ') {
+                plane?.setRotationFromEuler(new Euler(0, 0, 0))
+            }
+        }
+        const onKeyup = (event: KeyboardEvent) => {
+            if (event.key === 'w' || event.key === 'W') {
+                keydownTimestamp.pZ = 0
+            }
+            if (event.key === 's' || event.key === 'S') {
+                keydownTimestamp.nZ = 0
+            }
+            if (event.key === 'a' || event.key === 'A') {
+                keydownTimestamp.pX = 0
+            }
+            if (event.key === 'd' || event.key === 'D') {
+                keydownTimestamp.nX = 0
+            }
+            if (event.key === 'q' || event.key === 'Q') {
+                keydownTimestamp.pY = 0
+            }
+            if (event.key === 'e' || event.key === 'E') {
+                keydownTimestamp.nY = 0
+            }
+        }
+        document.addEventListener('keydown', onKeydown)
+        document.addEventListener('keyup', onKeyup)
+
         // render loop
 
         let cancelled = false
-        let lastFrameTime = performance.now()
-        const render = () => {
+        const frameTimes: number[] = []
+        let lastFrameClock = performance.now()
+        let totalFrameTime = 0
+        const render = (now: DOMHighResTimeStamp) => {
             if (cancelled) {
                 return
             }
 
             requestAnimationFrame(render)
 
-            const now = performance.now()
-            const delta = now - lastFrameTime
-            lastFrameTime = now
-            propeller?.rotation.set(propeller?.rotation.x + 0.01 * delta, 0, 0)
+            const delta = now - lastFrameClock
+            lastFrameClock = now
+
+            propeller?.rotateX(degToRad(delta))
+
+            if (plane) {
+                const step = 0.0001
+
+                plane.rotateX(
+                    (keydownTimestamp.nX !== 0 ? -(performance.now() - keydownTimestamp.nX) * step : 0) +
+                        (keydownTimestamp.pX !== 0 ? (performance.now() - keydownTimestamp.pX) * step : 0)
+                )
+                plane.rotateY(
+                    (keydownTimestamp.nY !== 0 ? -(performance.now() - keydownTimestamp.nY) * step : 0) +
+                        (keydownTimestamp.pY !== 0 ? (performance.now() - keydownTimestamp.pY) * step : 0)
+                )
+                plane.rotateZ(
+                    (keydownTimestamp.nZ !== 0 ? -(performance.now() - keydownTimestamp.nZ) * step : 0) +
+                        (keydownTimestamp.pZ !== 0 ? (performance.now() - keydownTimestamp.pZ) * step : 0)
+                )
+            }
 
             controls.update()
             renderer.render(scene, camera)
+
+            totalFrameTime += delta
+            frameTimes.push(delta)
+
+            if (totalFrameTime > 1000) {
+                const fps = (frameTimes.length / totalFrameTime) * 1000
+                const fAvg = frameTimes.reduce((a, b) => a + b, 0) / frameTimes.length
+                const fVar = Math.sqrt(frameTimes.reduce((a, b) => a + Math.pow(b - fAvg, 2), 0) / frameTimes.length)
+
+                const innerText = `fps=${fps.toFixed(2)}, avg=${fAvg.toFixed(2)}, var=${fVar.toFixed(2)}`
+                if (fpsIndicator.current) fpsIndicator.current.innerText = innerText
+
+                totalFrameTime = 0
+                frameTimes.length = 0
+            }
         }
-        render()
+        render(performance.now())
 
         return () => {
             cancelled = true
 
+            document.removeEventListener('keydown', onKeydown)
+            document.removeEventListener('keyup', onKeyup)
             resizeObserver.disconnect()
             controls.dispose()
             renderer.dispose()
@@ -100,6 +192,8 @@ const PropellerPage = () => {
             <canvas className="canvas" ref={canvas}>
                 <p>The canvas element is not supported by your browser</p>
             </canvas>
+
+            <span className="fps-indicator" ref={fpsIndicator} />
 
             <style jsx>
                 {`
@@ -116,10 +210,12 @@ const PropellerPage = () => {
                         width: 100%;
                     }
 
-                    .fps {
-                        color: white;
+                    .fps-indicator {
+                        background: black;
+                        color: green;
                         font-family: monospace;
-                        right: 0;
+                        font-size: 2vh;
+                        left: 0;
                         position: absolute;
                         top: 0;
                     }
